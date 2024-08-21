a9a87b2f5658b3aecd2218be75eddedb409adb85
Koukyosyumei <koukyosyumei@hotmail.com>
2024-08-21 09:21:43 -0400

    update

--- Fuzzification/BranchExtractorPass.cpp
+++ Fuzzification/BranchExtractorPass.cpp
@@ -5,29 +5,61 @@
 
 using namespace llvm;
 
-namespace {
-struct BranchExtractorPass : public FunctionPass {
-  static char ID;
-  BranchExtractorPass() : FunctionPass(ID) {}
+namespace
+{
+    struct BranchExtractorPass : public FunctionPass
+    {
+        static char ID;
+        BranchExtractorPass() : FunctionPass(ID) {}
 
-  bool runOnFunction(Function &F) override {
-    errs() << "Function: " << F.getName() << "\n";
+        bool runOnFunction(Function &F) override
+        {
+            errs() << "Function: " << F.getName() << "\n";
 
-    // Loop through each basic block in the function
-    for (BasicBlock &BB : F) {
-      // Loop through each instruction in the basic block
-      for (Instruction &I : BB) {
-        // Check if the instruction is a BranchInst
-        if (BranchInst *BI = dyn_cast<BranchInst>(&I)) {
-          errs() << "  Found BranchInst: " << *BI << "\n";
+            // Loop through each basic block in the function
+            for (BasicBlock &BB : F)
+            {
+                // Loop through each instruction in the basic block
+                for (Instruction &I : BB)
+                {
+                    // Check if the instruction is a BranchInst
+                    if (BranchInst *BI = dyn_cast<BranchInst>(&I))
+                    {
+                        errs() << "  Found BranchInst: " << *BI << "\n";
+
+                        if (BI->isConditional() && isa<CmpInst>(BI->getCondition()))
+                        {
+                            CmpInst *CI = dyn_cast<CmpInst>(BI->getCondition());
+                            auto *opA = CI->getOperand(0);
+                            auto *opB = CI->getOperand(1);
+
+                            // assuming both operands are of the same type for conditional
+                            errs() << "    Conditional Branch: \n";
+                            if (opA->getType()->isDoubleTy())
+                            {
+                                errs() << "      double\n";
+                            }
+                            else if (opA->getType()->isIntegerTy(32))
+                            {
+                                errs() << "      integer\n";
+                            }
+                            else if (opA->getType()->isFloatTy())
+                            {
+                                errs() << "      float\n";
+                            }
+                            else
+                            {
+                                errs() << "      other types\n";
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Since this pass does not modify the function, return false
+            return false;
         }
-      }
-    }
-
-    // Since this pass does not modify the function, return false
-    return false;
-  }
-};
+    };
 } // namespace
 
 char BranchExtractorPass::ID = 0;
