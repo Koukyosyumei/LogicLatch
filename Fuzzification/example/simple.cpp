39851e194574a12ddf4c76cf4d3bc0417a151392
Koukyosyumei <koukyosyumei@hotmail.com>
2024-08-21 09:43:21 -0400

    update

--- Fuzzification/example/simple.cpp
+++ Fuzzification/example/simple.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <cstring>
+#include <string>
 #include <iostream>
 
 int main(int argc, char **argv)
 {
     int x = 0;
+    std::string y;
     std::cin >> x;
+    std::cin >> y;
 
     int i = 0;
+    int j = 0;
+
     if (x == 0)
     {
         i = 1;
@@ -15,6 +21,13 @@ int main(int argc, char **argv)
     {
         i = 2;
     }
-    std::cout << i << std::endl;
+
+    if (!std::strcmp(y.c_str(), "pass")) {
+        j = 3;
+    } else {
+        j = 4;
+    }
+
+    std::cout << i << " " << j << std::endl;
     return 0;
 }
